4b7314dc53c6ac8f98bb5e3a318b327bff30c694
EC2 Default User <ec2-user@ip-172-31-28-10.us-east-2.compute.internal>
2018-11-03 06:12:19 +0000

    テスト2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,8 +33,6 @@ Rails.application.configure do
   
   
   
-  
-  
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
